70b7ffb2a5d6aff86d9be3f81df213e31ef223a9
harrybello14@gmail.com <harrybello14@gmail.com>
2023-10-27 15:10:07 +0100

    second comit

--- src/Pages/Home/TicTacToeModules/cell.jsx
+++ src/Pages/Home/TicTacToeModules/cell.jsx
@@ -1,35 +1,73 @@
 /* eslint-disable react/prop-types */
-const Cell = ({id, cell, setCells, turn, setTurn,cells,winningMessage}) => {
-
-  const handleClick = (e) => {
-    const taken = e.target.firstChild.classList.contains("circle") || e.target.firstChild.classList.contains("cross")
-    if(!taken){
-      if(turn === "circle"){
-        e.target.firstChild.classList.add("circle")
-        handleCellChange("circle")
-        setTurn("cross")
-      }
-      if(turn === "cross"){
-        e.target.firstChild.classList.add("cross")
-        handleCellChange("cross")
-        setTurn("circle")
-      }
-    }
-  }
-
-  const handleCellChange = (className) => {
-    const nextCells = cells.map((cell,index)=>{
-      if(index === id) {
-        return className
-      }else {
-        return cell
+const Cell = ({id, cell, setCells, turn, setTurn,cells,winningMessage,setWinningMessage}) => {
+const checkWin = () => {
+  const winningCombos = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+  ]
+    winningCombos.forEach(array => {
+      let circleWins = array.every(cell => cells[cell] === "O")
+      if(circleWins) {
+        setWinningMessage("Circle Wins!")
+        return
       }
     })
-    setCells(nextCells)
+    winningCombos.forEach(array => {
+      let crossWins = array.every(cell => cells[cell] === "X")
+      if(crossWins) {
+        setWinningMessage("Cross Wins!")
+        return
+      }
+    })
+}
+const checkForDraw = (cells) => {
+ let Draw = cells.every((cell) => cell !== '') && !checkWin(cells);
+ if(Draw){
+  setWinningMessage("Draw!")
+  return
+ }
+};
+  const handleClick = () => {
+    console.log(winningMessage)
+      if(cells[id] !== "" || winningMessage !== null) return
+      
+
+    // for(let i = 0; i < cells.length; i++){
+      if(turn === "O"){
+        const newCells = cells
+        newCells[id] = "O"
+        setCells(newCells)
+        // handleCellChange("O")
+        setTurn("X")
+      }else{
+        const newCells = cells
+        newCells[id] = "X"
+        setCells(newCells)
+        // handleCellChange("X")
+        setTurn("O")
+      }
+      checkWin()
+      checkForDraw(cells)
+    // }
+    
+      
+    
   }
+
+  // const handleCellChange = (className) => {
+  //   const nextCells = cells.map((cell,index)=>{
+  //     if(index === id) {
+  //       return className
+  //     }else {
+  //       return cell
+  //     }
+  //   })
+  //   setCells(nextCells)
+  // }
   return (
-    <div className="square" id={id} onClick={!winningMessage  && handleClick}>
-      <div className={cell}></div>
+    <div className="square" id={id} onClick={handleClick}>
+      <div className="cell-item">{cell}</div>
     </div>
   );
 }
